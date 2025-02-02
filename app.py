8d16675a64ed5cd7995c001309abbfd94e4ba963
DevCami <devcode.bycami@gmail.com>
2025-02-02 01:09:19 +0100

    Actualizando el requeremts y primeros pasos con la app

---
+++ app.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+import ttkbootstrap as ttk
+
+app = ttk.Window(themename="darkly")
+
+# Crear la ventana
+
+app.title("Transcripcion beta, DvCami")
+app.geometry("600x400+256+256")
+
+app.mainloop()
+
