e50289295f037c73ef88e9c67be3fc8a4c9293ed
DevCami <devcode.bycami@gmail.com>
2025-02-02 14:20:17 +0100

    Cambios antes de que richard haga algo

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ app = ttk.Window(themename="darkly")
 
 app.title("Transcripcion beta, DvCami")
 app.geometry("1300x700+256+256")
+app.resizable(False,False)
 
+# Cuerpo de la ventana
+
+frame_drop = tk.Frame(master = app, width=300, height= 300, padx=10, pady= 10)
+frame_drop.pack()
+frame_drop.config(background = "lightgreen") # Fondo del Frame en blanco
 
 app.mainloop()
 
