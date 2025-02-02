ebae06921c0f0657f3456c2a932b69614af96deb
DevCami <devcode.bycami@gmail.com>
2025-02-02 13:55:04 +0100

    Importando la libreria nueva y redimendionando la ventanda

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
 import ttkbootstrap as ttk
+from tkinterdnd2 import TkinterDnD, DND_FILES
 
 app = ttk.Window(themename="darkly")
+
 
 # Crear la ventana
 
 app.title("Transcripcion beta, DvCami")
-app.geometry("800x700+256+256")
+app.geometry("1300x700+256+256")
 
 
 app.mainloop()
